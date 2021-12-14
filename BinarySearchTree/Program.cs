0fdec2bb390f3bfc7609f1692f7c1dabc57a06d6
Md aamir reza <aamirreza156@gamil.com>
2021-12-14 17:19:27 +0530

    [Aamir15] Add : Added CreateBSTAsShownInFigure

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -4,13 +4,30 @@ class program
 {
 	public static void Main(string[] args)
 	{
+		//BinarySearchTree tree = new BinarySearchTree();
+		//tree.insert(50);
+		//tree.insert(30);
+		//tree.insert(70);
+		//tree.modifyBST(tree.root);
+		//// print inorder traversal of the modified BST
+		//tree.inorder();
 		BinarySearchTree tree = new BinarySearchTree();
-		tree.insert(50);
+		tree.insert(56);
 		tree.insert(30);
 		tree.insert(70);
+		tree.insert(22);
+		tree.insert(40);
+		tree.insert(60);
+		tree.insert(95);
+		tree.insert(11);
+		tree.insert(65);
+		tree.insert(3);
+		tree.insert(16);
+		tree.insert(63);
+		tree.insert(67);
 		tree.modifyBST(tree.root);
-		// print inorder traversal of the modified BST
 		tree.inorder();
+
 
 
 
